7abfeb081b298b020622b599181eaf98d9bf0363
Abdo_PC <Abdo_PC@DESKTOP-5ODQKSV>
2025-04-17 17:31:25 +0200

    Modified

---
+++ Controllers/InfoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstTry.Controllers
+{
+    public class InfoController : Controller
+    {
+        public ActionResult Aboutus()
+        {
+            return View();
+        }
+        public IActionResult PublisherPartnership()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+
+       
+    }
+}
